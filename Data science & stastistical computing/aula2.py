8cec073f0620f11c6048fd132238579ec5fc81d2
vitor12123 <vitorpallis@gmail.com>
2026-03-05 22:24:04 -0300

    ADD: aula 2 de data science

---
+++ Data science & stastistical computing/aula2.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+x = [1,2,3,4,5]
+
+y = [10,20,30,40,50]
+
+plt.plot(x,y)
+plt.xlabel("eixo x")
+plt.ylabel("eixo y")
+plt.title("meu primeiro grafico")
+plt.show()
+
+# grafico de barra 
+
+x2 = ["A","B","C","D"]
+
+y2 = [3,7,5,2]
+
+plt.bar(x2,y2)
+plt.show()
+
+# heatmap
+
+#multigraficos
+
