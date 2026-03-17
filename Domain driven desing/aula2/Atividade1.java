35f432361d2c332ef0c916bf9371c138f983cbca
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-16 22:34:29 -0300

    ADD: aula 2 de Domain Drive Desing

---
+++ Domain driven desing/aula2/Atividade1.java
@@ -0,0 +1,14 @@
+public class Atividade1 {
+    // atributos
+    private String variavel1;
+    private int variavel2;
+
+    //metodos
+     public void setVariavel1(String variavel1){
+         this.variavel1 = variavel1;
+     }
+
+    public String getVariavel1() {
+        return variavel1;
+    }
+}
---
+++ Domain driven desing/aula3/MainAula3.java
@@ -0,0 +1,5 @@
+public class MainAula3 {
+    public static void main(String[] args) {
+
+    }
+}
