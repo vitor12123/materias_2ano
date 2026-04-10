974a5432b01574a31bb84d93a7fcbdb9bb299d45
vitor12123 <vitorpallis@gmail.com>
2026-04-09 21:03:15 -0300

    Git pull percentil

--- Data science & stastistical computing/Pencertil.py
+++ Data science & stastistical computing/Pencertil.py
@@ -1,3 +1,17 @@
+lista = [3,14,43,95,34,321,54,67,89,100]
 
+lista.sort()
+print(lista)
 
+def percentil(list,p):
+    percentil = (p/100) * (len(list) -1)
+    if percentil % 1 != 0:
+        p1 = int(percentil)
+        percentilCpaCerto = (list[p1] + list[p1+1])/2
+        
+        return percentilCpaCerto
+    else:
+        return list[int(percentil)]
 
+print(percentil(lista,25))
+print(percentil(lista,75))
